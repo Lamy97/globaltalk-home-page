dc36396f1ac3af1c5300e0b42945493f9e929df7
Lamy97 <lamyae.rjafallah@ump.ac.ma>
2024-03-06 19:22:03 +0100

    responsive adjust

---
+++ Javascript/JS-Home.js
@@ -0,0 +1,7 @@
+
+      const menuBtn = document.querySelector('.menu-btn');
+      const menu = document.querySelector('.titles');
+      
+      menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+      });
