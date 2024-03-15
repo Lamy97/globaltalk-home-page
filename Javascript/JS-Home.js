2bb3451c86bd39b77bd55eaaa6ff07a2c361483a
Lamy97 <lamyae.rjafallah@ump.ac.ma>
2024-03-15 22:00:09 +0000

    Adjust responsive

--- Javascript/JS-Home.js
+++ Javascript/JS-Home.js
@@ -1,7 +1,20 @@
 
+      //Burger Menu
       const menuBtn = document.querySelector('.menu-btn');
       const menu = document.querySelector('.titles');
       
       menuBtn.addEventListener('click', () => {
         menu.classList.toggle('active');
       });
+     //Resise the writing in the 1st section
+      // window.addEventListener('resize', function() {
+      //   const width = window.innerWidth;
+      
+      //   if (width <= 750) {
+      //     document.querySelector('.Learning').style.fontSize = '16px';
+      //   } else if (width <= 1024) {
+      //     document.querySelector('.Learning').style.fontSize = '70px';
+      //   } else {
+      //     document.querySelector('.Learning').style.fontSize = '56px';
+      //   }
+      // });
